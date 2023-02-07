17552a02eaea7c02c028b0d1313fce130e6dfa41
Иван Петров <fatevector@yandex.ru>
2023-02-07 22:12:16 +0300

    functions composition

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {compose, pipe} from 'lodash/fp';
 
 const App = params => {
-  const someFn = () => () => 'app'
-  const fn = someFn()
-  return <h1>{fn()}</h1>
+  const x = 2
+  const double = num => num * 2
+  const square = num => num*num
+  const half = num => num / 2
+  const mathCalculate=pipe(double, square, half)
+  return <h1>{mathCalculate(x)}</h1>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
