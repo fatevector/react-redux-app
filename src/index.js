b437948cae5fb80c9b61a25d5d350da176ea8bda
Иван Петров <fatevector@yandex.ru>
2023-02-08 12:10:14 +0300

    add dispatch for myRedux

--- src/index.js
+++ src/index.js
@@ -4,13 +4,23 @@ import ReactDOM from 'react-dom/client';
 const createStore = (initialState) => {
   let state = initialState
   const getState = () => state
-  return {getState}
+  const dispatch = (action) => {
+    console.log(action);
+    if (action.type === 'task/completed') {
+      const newArray = [...state]
+      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+      newArray[elementIndex].completed = true
+      state = newArray
+      console.log(state);
+    }
+  }
+  return {getState, dispatch}
 }
 const store = createStore([{id: 1, description: 'task 1', completed: false}])
 
 const App = params => {
   console.log(store.getState());
-  return <h1>App</h1>
+  return <><h1>App</h1><button onClick={() => store.dispatch({type: 'task/completed', payload: {id: 1}})}>Complete</button></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
