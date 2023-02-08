0dc22411682991d2c6245e2318d9704499030cd4
Иван Петров <fatevector@yandex.ru>
2023-02-08 12:24:39 +0300

    add taskReducer for myRedux

--- src/index.js
+++ src/index.js
@@ -1,31 +1,40 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const createStore = (initialState) => {
-  let state = initialState
-  const getState = () => state
-  const dispatch = (action) => {
-    console.log(action);
-    if (action.type === 'task/completed') {
-      const newArray = [...state]
-      const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
-      newArray[elementIndex].completed = true
-      state = newArray
-      console.log(state);
+const taskReducer = (state, action) => {
+    switch (action.type) {
+    case 'task/completed':
+        const newArray = [...state]
+        const elementIndex = newArray.findIndex(el => el.id === action.payload.id)
+        newArray[elementIndex].completed = true
+        return newArray
+    default:
+        break;
     }
-  }
-  return {getState, dispatch}
 }
-const store = createStore([{id: 1, description: 'task 1', completed: false}])
+
+const createStore = (reducer, initialState) => {
+    let state = initialState
+    const getState = () => state
+    const dispatch = (action) => {
+        state = reducer(state, action)
+    }
+    return {getState, dispatch}
+}
+const store = createStore(taskReducer, [{id: 1, description: 'task 1', completed: false}])
 
 const App = params => {
-  console.log(store.getState());
-  return <><h1>App</h1><button onClick={() => store.dispatch({type: 'task/completed', payload: {id: 1}})}>Complete</button></>
+    const completeTask = () => {
+        store.dispatch({type: 'task/completed', payload: {id: 1}})
+        console.log(store.getState());
+    }
+    console.log(store.getState());
+    return <><h1>App</h1><button onClick={completeTask}>Complete</button></>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
